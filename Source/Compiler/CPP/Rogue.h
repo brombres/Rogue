497bda7b17a3119d811bd9f4bdd417830d3d2c58
Abe Pralle <abe.pralle@gmail.com>
2015-01-21 18:59:50 -0800

    Consolidated RogueTypes and RogueAllocator into Rogue.h/cpp.

--- Source/Compiler/CPP/Rogue.h
+++ Source/Compiler/CPP/Rogue.h
@@ -11,7 +11,103 @@
 //  under the terms of the UNLICENSE ( http://unlicense.org ).
 //=============================================================================
 
-#include "RogueTypes.h"
+#if defined(_WIN32)
+#  include <windows.h>
+#else
+#  include <cstdint>
+#endif
+
+#if defined(_WIN32)
+  typedef double           RogueReal;
+  typedef float            RogueFloat;
+  typedef __int64          RogueLong;
+  typedef __int32          RogueInteger;
+  typedef unsigned __int16 RogueCharacter;
+  typedef unsigned char    RogueByte;
+  typedef RogueInteger     RogueLogical;
+#else
+  typedef double           RogueReal;
+  typedef float            RogueFloat;
+  typedef int64_t          RogueLong;
+  typedef int32_t          RogueInteger;
+  typedef uint16_t         RogueCharacter;
+  typedef uint8_t          RogueByte;
+  typedef RogueInteger     RogueLogical;
+#endif
+
+struct RogueAllocator;
+
+#define ROGUE_TRACE( obj ) \
+{ \
+  if (obj && obj->size >= 0) \
+  { \
+    obj->size = ~obj->size; \
+    obj->type->trace( obj ); \
+  } \
+}
+
+//-----------------------------------------------------------------------------
+//  RogueType
+//-----------------------------------------------------------------------------
+struct RogueObject;
+
+struct RogueType
+{
+  virtual ~RogueType() {}
+  virtual RogueObject* create_object() = 0;
+  virtual const char*  name() = 0;
+  virtual void         trace( RogueObject* obj ) {}
+};
+
+
+//-----------------------------------------------------------------------------
+//  RogueObject
+//-----------------------------------------------------------------------------
+struct RogueObject
+{
+  RogueObject* next_object;
+  // Used to keep track of this allocation so that it can be freed when no
+  // longer referenced.
+
+  RogueType*   type;
+  // Type info for this object.
+
+  RogueInteger size;
+  // Set to be ~size when traced through during a garbage collection,
+  // then flipped back again at the end of GC.
+
+  RogueInteger reference_count;
+  // A positive reference_count ensures that this object will never be
+  // collected.  A zero reference_count means this object is kept as long as
+  // it is visible to the memory manager.
+
+  RogueObject* retain()  { ++reference_count; return this; }
+  void         release() { --reference_count; }
+};
+
+
+//-----------------------------------------------------------------------------
+//  RogueString
+//-----------------------------------------------------------------------------
+struct RogueStringType : RogueType
+{
+  RogueObject* create_object()
+  {
+    return 0;  // not used
+  }
+
+  const char* name() { return "String"; }
+};
+
+struct RogueString : RogueObject
+{
+  RogueInteger   count;
+  RogueInteger   hash_code;
+  RogueCharacter characters[1];
+
+  static RogueString* create( const char* c_string, int count=-1 );
+  static void         println( RogueString* st );
+};
 
 //-----------------------------------------------------------------------------
 //  RogueProgramCore
@@ -32,5 +128,75 @@ struct RogueProgramCore
 
   void         collect_garbage();
 };
+
+//-----------------------------------------------------------------------------
+//  RogueAllocator
+//-----------------------------------------------------------------------------
+#ifndef ROGUEMM_PAGE_SIZE
+// 4k; should be a multiple of 256 if redefined
+#  define ROGUEMM_PAGE_SIZE (4*1024)
+#endif
+
+// 0 = large allocations, 1..4 = block sizes 64, 128, 192, 256
+#ifndef ROGUEMM_SLOT_COUNT
+#  define ROGUEMM_SLOT_COUNT 5
+#endif
+
+// 2^6 = 64
+#ifndef ROGUEMM_GRANULARITY_BITS
+#  define ROGUEMM_GRANULARITY_BITS 6
+#endif
+
+// Block sizes increase by 64 bytes per slot
+#ifndef ROGUEMM_GRANULARITY_SIZE
+#  define ROGUEMM_GRANULARITY_SIZE (1 << ROGUEMM_GRANULARITY_BITS)
+#endif
+
+// 63
+#ifndef ROGUEMM_GRANULARITY_MASK
+#  define ROGUEMM_GRANULARITY_MASK (ROGUEMM_GRANULARITY_SIZE - 1)
+#endif
+
+// Small allocation limit is 256 bytes - afterwards objects are allocated
+// from the system.
+#ifndef ROGUEMM_SMALL_ALLOCATION_SIZE_LIMIT
+#  define ROGUEMM_SMALL_ALLOCATION_SIZE_LIMIT  ((ROGUEMM_SLOT_COUNT-1) << ROGUEMM_GRANULARITY_BITS)
+#endif
+
+
+struct RogueAllocationPage
+{
+  // Backs small 0..256-byte allocations.
+  RogueAllocationPage* next_page;
+
+  RogueByte* cursor;
+  int        remaining;
+
+  RogueByte  data[ ROGUEMM_PAGE_SIZE ];
+
+  RogueAllocationPage( RogueAllocationPage* next_page );
+
+  void* allocate( int size );
+};
+
+//-----------------------------------------------------------------------------
+//  RogueAllocator
+//-----------------------------------------------------------------------------
+struct RogueAllocator
+{
+  RogueAllocationPage* pages;
+  RogueObject*           free_objects[ROGUEMM_SLOT_COUNT];
+
+  RogueAllocator();
+  ~RogueAllocator();
+  void* allocate( int size );
+  void* allocate_permanent( int size );
+  void* free( void* data, int size );
+  void* free_permanent( void* data, int size );
+};
+
+extern RogueAllocator Rogue_allocator;
+
+
 
 #include "RogueProgram.h"
