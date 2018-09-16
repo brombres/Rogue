9596285c1a498032573145a8aa2a270fde154ea7
Abe Pralle <Abe.Pralle@gmail.com>
2018-09-15 23:21:26 -0700

    [RogueC] Reworked type and string config to use tables and loops
    - Was potentially thousands of lines of setup.
    - Made emscripten launch slow.
    - Reworked to use data tables and initialization loops.

--- Source/Libraries/Standard/NativeCPP.cpp
+++ Source/Libraries/Standard/NativeCPP.cpp
@@ -583,6 +583,8 @@ int Rogue_allocation_bytes_until_gc = Rogue_gc_threshold;
 //-----------------------------------------------------------------------------
 //  Misc Utility
 //-----------------------------------------------------------------------------
+void Rogue_define_literal_string( int index, const char* st, int count=-1 );
+
 void Rogue_define_literal_string( int index, const char* st, int count )
 {
   Rogue_literal_strings[index] = (RogueString*) RogueObject_retain( RogueString_create_from_utf8( st, count ) );
