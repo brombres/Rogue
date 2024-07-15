0f09c532994d289c32ce355c2205191577a8b870
Brom Bresenham <brom.bresenham@runegate.com>
2024-07-14 20:47:15 -0400

    [RogueC] Split C source files for faster compilation

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @if not exist Build ( mkdir Build )
-cl /O2 /nologo /bigobj Source\RogueC\Bootstrap\RogueC.c /FoBuild\roguec.obj /FeBuild\roguec.exe
+cl /O2 /nologo /bigobj Source/RogueC/Bootstrap/RogueC-1.c Source/RogueC/Bootstrap/RogueC-2.c Source/RogueC/Bootstrap/RogueC-3.c Source/RogueC/Bootstrap/RogueC-4.c Source/RogueC/Bootstrap/RogueC-5.c Source/RogueC/Bootstrap/RogueC-6.c Source/RogueC/Bootstrap/RogueC-7.c Source/RogueC/Bootstrap/RogueC-8.c Source/RogueC/Bootstrap/RogueC-9.c Source/RogueC/Bootstrap/RogueC-10.c Source/RogueC/Bootstrap/RogueC-11.c /FoBuild\roguec.obj /FeBuild\roguec.exe
 
 @if exist Build\Libraries ( rmdir /S /Q Build\Libraries )
 @if not exist Build\Libraries ( mkdir Build\Libraries )
